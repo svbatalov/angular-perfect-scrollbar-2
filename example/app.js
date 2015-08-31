b0c5d86f88b04c84a14b06d40eca54f14586fbcb
Batalov Sergey <firstfire@list.ru>
2015-09-01 00:18:53 +0500

    Initial commit

---
+++ example/app.js
@@ -0,0 +1,28 @@
+var angular = require('angular');
+
+angular.module('app', [
+  require('../')
+])
+.controller('ctrl', ['$scope', '$interval', function ($scope, $interval) {
+  $scope.scrollopts = {
+    wheelSpeed: 2,
+    wheelPropagation: true,
+    minScrollbarLength: 20
+  };
+  $scope.list = [];
+  $scope.show = true;
+  var counter = 0;
+  var direction = true;
+  var timer = $interval(function () {
+    var len = $scope.list.length;
+    if ( (len === 0 && !direction) || len === 50) {
+      direction = !direction;
+    }
+    if (direction) {
+      $scope.list.push('line' + (counter++));
+    } else {
+      $scope.list.pop();
+      counter--;
+    }
+  }, 100, 50);
+}]);
---
+++ index.js
@@ -0,0 +1,48 @@
+var Ps = require('perfect-scrollbar');
+//var angular = require('angular');
+require('./app.css');
+
+module.exports = 'angular-perfect-scrollbar-2';
+
+angular.module(module.exports, [])
+.directive('perfectScrollbar', ['$parse', function ($parse) {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    template: '<div><div ng-transclude></div></div>',
+    replace: true,
+    link: function (scope, elem, attr) {
+      var scroll_opts = $parse(attr['opts'])(scope);
+      var el = elem[0];
+
+      Ps.initialize(el, scroll_opts);
+
+      obs_opts = {
+        childList: true,
+        subtree: true,
+        characterData: true,
+      };
+
+      if(MutationObserver) {
+        obs = new MutationObserver(function () {
+          Ps.update(el);
+        });
+        obs.observe(el, obs_opts);
+      } else {
+        console.warn(module.exports + ': Your browser does not support MutationObserver.')
+      }
+
+      scope.$watch('update', function (n, o) {
+        if (n) {
+          Ps.update(el);
+          scope.update = false;
+        }
+      });
+
+      scope.$on('destroy', function () {
+        obs.disconnect();
+        Ps.destroy(el);
+      });
+    }
+  };
+}]);
